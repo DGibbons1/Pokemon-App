0e7ede655353a16d95a313e4750ae761fb2a50fd
D_Gibbons <34580928+DGibbons1@users.noreply.github.com>
2019-09-23 21:48:30 +0100

    basic navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'pokemon-app';
+  public currentPage: string = "home-page";
+
+  //Method to open the home page
+  public openHomePage(){
+    this.currentPage = "home-page";
+  }
+
+  //Method to open the Pokemon List page
+  public openListPage(){
+    this.currentPage = "list-page";
+  }
+
+  //Method to open the Pokemon detail page
+  public openDetailPage(pokemonID: number){
+    this.currentPage = "detail-page";
+  }
+
+
+
+
 }
