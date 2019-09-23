a32dd10280a9abfd8e39b9087813c242e72144cb
D_Gibbons <34580928+DGibbons1@users.noreply.github.com>
2019-09-23 20:45:09 +0100

    Add home, list, and detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ListPageComponent } from './list-page/list-page.component';
+import { DetailPageComponent } from './detail-page/detail-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ListPageComponent,
+    DetailPageComponent
   ],
   imports: [
     BrowserModule,
